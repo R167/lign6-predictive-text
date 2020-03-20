51df6618c92b5fb587b9bfc71a60952be090054e
Thomas Lauer <tlauer@ucsd.edu>
2020-03-20 12:23:38 -0500

    Now using regex to remove all non letters

--- predict/loader.py
+++ predict/loader.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 
 nltk.download('punkt')
 
@@ -8,11 +9,19 @@ class Tokenizer:
       contents = f.read()
       self.sentences = self.tokenize_sentences(contents)
 
-  def tokenize_sentences(self, contents):
-    sentences = []
+  def clean(self, contents):
     # get rid of smart quotes
     # sorry about wasting ram
-    contents = contents.replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c","").replace(u"\u201d", "")
+    # contents = contents.replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c","").replace(u"\u201d", "")
+
+    # jk using regular expressions
+    return re.sub(r'[^A-Za-z]', ' ', contents)
+
+  def tokenize_sentences(self, contents):
+    sentences = []
+
+    contents = self.clean(contents)
+
     for sent in nltk.sent_tokenize(contents):
       sentences.append(nltk.word_tokenize(sent))
 
