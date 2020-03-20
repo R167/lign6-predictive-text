6c437c2a88982a9517be3b55122db62fee8dc839
Winston Durand <me@winstondurand.com>
2020-03-20 11:20:49 -0700

    un borked

--- predict/ngrams.py
+++ predict/ngrams.py
@@ -7,13 +7,13 @@ class Parser:
         self.ngrams = {}
 
     def parse(self, sentences):
-        for s in sentences:
-            for i in range(len(s) - self.ngram_size):
-                # convert the list of grams into a string
-                sequence = " ".join(s[i:i+self.ngram_size])
-                if sequence not in self.ngrams.keys():
-                    self.ngrams[sequence] = []
-                self.ngrams[sequence].append(s[i+self.ngram_size])
+        s = sentences
+        for i in range(len(s) - self.ngram_size):
+            # convert the list of grams into a string
+            sequence = " ".join(s[i:i+self.ngram_size])
+            if sequence not in self.ngrams.keys():
+                self.ngrams[sequence] = []
+            self.ngrams[sequence].append(s[i+self.ngram_size])
 
         return self.ngrams
 
